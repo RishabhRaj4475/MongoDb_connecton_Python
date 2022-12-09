ced2af52a23389898ea1c40bbb89807146651a98
Rishabh Raj <rishabh4475@gmail.com>
2022-12-10 01:25:52 +0530

    first commit

---
+++ pymongo_test_query.py
@@ -0,0 +1,16 @@
+from pymongo_get_database import get_database
+from pandas import DataFrame
+
+dbname = get_database()
+ 
+# Create a new collection
+collection_name = dbname["user_1_items"]
+ 
+item_details = collection_name.find()
+
+   # convert the dictionary objects to dataframe
+items_df = DataFrame(item_details)
+
+    # see the magic
+print(items_df)
+
